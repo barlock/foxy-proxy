6748fb14745505cff4218a2858af2b6fe3faed90
Michael Barlock <barlockm@gmail.com>
2015-05-18 22:37:27 -0400

    Parse html doc

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-var request = require("request"),
-    http = require("http");
+var cheerio = require("cheerio"),
+    express = require("express"),
+    request = require("request"),
+    _ = require("underscore");
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080,
+    app = express();
 
-var server = http.createServer(function (req, resp) {
-    var url = req.url.substr(1);
+app.use(function (req, res) {
+    var url = req.query.url;
+    req.url = "";
 
     if (url) {
-        req.pipe(request(url)).pipe(resp)
+        if(url.match(/\.(css|js|png|gif|jpeg|jpg)$/)) {
+            request(decodeURIComponent(url)).pipe(res);
+        } else {
+            request(decodeURI(url), function(err, response, body) {
+                var $ = cheerio.load(body),
+                    host = req.protocol + '://' + req.get('Host') + req.url;
+
+                $("[href]").each(function () {
+                    var elem = $(this);
+                    elem.attr("href", host + "/?url=" + encodeURIComponent(elem.attr("href")))
+                });
+
+                $("[src]").each(function () {
+                    var elem = $(this);
+                    elem.attr("src", host + "/?url=" + encodeURIComponent(elem.attr("src")))
+                });
+
+                _.each(response.headers, function(value, key) {
+                    if(typeof value === "string" && key !== "content-security-policy") {
+                        res.setHeader(key, value);
+                    }
+                });
+                res.send($.html()).end();
+            });
+        }
+    } else {
+        req.send("NO! add a url query! like ?url=https://github.com");
     }
 });
 
-server.listen(port);
+app.listen(port);
